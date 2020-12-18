e393b00c6574e4cf3043359773a40deb454f30fd
JackWinterburn <jack.winterburn4@hotmail.co.uk>
2020-12-18 19:55:53 +0000

    youtube player loaded in

--- main.go
+++ main.go
@@ -7,6 +7,19 @@ import (
 
 var templates = template.Must(template.ParseFiles("templates/home.html"))
 
+/*
+ * TODO (Jack)
+ * Allow users to be able to create a room
+ * that is accessable via a url
+ */
+
+/*
+ * TODO (Jack)
+ * Allow users to be able to input a youtube
+ * url and have that be synced across all clients
+ * (mess with web sockets)
+ */
+
 // Video used for javascript to be able to get the video requested
 type Video struct {
 	Title   string
