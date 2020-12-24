6a385afeb01dcc48b78b6d40a7c7a0544aa47f50
JackWinterburn <jack.winterburn4@hotmail.co.uk>
2020-12-24 23:53:18 +0000

    FIXME - fix 'postmessage' issue ASAP

--- static/src/components/Player.tsx
+++ static/src/components/Player.tsx
@@ -1,75 +1,45 @@
-import { useState, useRef, createContext } from "react";
-import YouTube, { Options } from 'react-youtube';
-import Controls from "./Controls";
-import { connect } from "../connection"
+import { useRef } from "react";
+import YouTube, { Options } from "react-youtube";
+import { connect, sendMsg } from "../connection";
 import "../scss/Player.scss";
 
-export const PlayerContext = createContext({ data: 0, time: 0, duration: 0 })
-
 export default function Player() {
-  //TODO (Jack): get rid of that disgusting "any" type
-  let YTref = useRef<any>(null)
+  let YTref = useRef<any>(null);
 
   connect((msg) => {
     let msgBody = JSON.parse(msg.data).body;
-    // switch (msgBody) {
-    //   case "play":
-    //     YTref.current.internalPlayer.playVideo()
-    //     break;
-    //   case "pause":
-    //     YTref.current.internalPlayer.pauseVideo()
-    //     break;
-    // };
-    if (msgBody === "play") {
-      YTref.current.internalPlayer.playVideo()
-    } else {
-      YTref.current.internalPlayer.pauseVideo()
+    console.log(msgBody);
+    switch (msgBody) {
+      case "play":
+        YTref.current.internalPlayer.playVideo();
+        break;
+      case "pause":
+        YTref.current.internalPlayer.pauseVideo();
+        break;
     }
   });
 
   const YTOptions: Options = {
-    height: '390',
-    width: '640',
+    height: "390",
+    width: "640",
+    host: "http://www.youtube.com",
     playerVars: {
       controls: 1,
-      listType: "playlist",
-      rel: 1,
-      autoplay: 1
+      rel: 0,
     },
   };
-
-
-  const [playerState, setPlayerState] = useState({
-    data: -1,
-    time: 0,
-    duration: 0
-  });
-
-  const onReady = (_: Event) => {
-  //  YTref.current.internalPlayer.playVideo()
-    console.log("ready")
-    // YTref.current.internalPlayer.pauseVideo()
-  }
 
   return (
     <div className="YT-player-wrapper">
       <YouTube
         ref={YTref}
         className="YT-player"
-        videoId="2g811Eo7K8U"
+        videoId="_2HIkR4aWxU"
         opts={YTOptions}
-        onReady={onReady}
-        onStateChange={(e: { target: any, data: number }): void => {
-          let newTime = e.target.getCurrentTime
-          let newDuration = e.target.getDuration
-          console.log(e.data)
-          setPlayerState(_ => ({ data: e.data, time: newTime, duration: newDuration }));
-          console.log(playerState.data)
-        }} />
+      />
 
-      <PlayerContext.Provider value={playerState}>
-        <Controls />
-      </PlayerContext.Provider>
+      <i className="fas fa-play" onClick={() => sendMsg("play")}></i>
+      <i className="fas fa-pause" onClick={() => sendMsg("pause")}></i>
     </div>
   );
 }
